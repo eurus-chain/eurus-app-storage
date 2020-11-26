500417e39968892518059c02adbdbe61f95a722c
Damon Leong <damon.leong@goldhub.hk>
2020-11-26 12:01:25 +0800

    Added test on in normal storage

--- lib/secure_storage.dart
+++ lib/secure_storage.dart
@@ -1,13 +1,12 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorageKit {
-  FlutterSecureStorage _storage = FlutterSecureStorage();
-
   /// Write a [String] variable [val] to Secure Storage with the key [key]
   ///
   /// Return [true] after writing the variable to Secure Storage
   Future<bool> setValue(String val, String key) async {
-    await _storage.write(key: key, value: val);
+    final FlutterSecureStorage storage = FlutterSecureStorage();
+    await storage.write(key: key, value: val);
     return true;
   }
 
@@ -16,7 +15,8 @@ class SecureStorageKit {
   /// Reture [value] in [String] if value was found
   /// Return [null] if is empty
   Future<String> readValue(String key) async {
-    String value = await _storage.read(key: key);
+    final FlutterSecureStorage storage = FlutterSecureStorage();
+    String value = await storage.read(key: key);
     return value;
   }
 
@@ -24,7 +24,8 @@ class SecureStorageKit {
   ///
   /// Return [true] after deleting the variable in Secure Storage
   Future<bool> deleteValue(String key) async {
-    await _storage.delete(key: key);
+    final FlutterSecureStorage storage = FlutterSecureStorage();
+    await storage.delete(key: key);
     return true;
   }
 
@@ -32,7 +33,8 @@ class SecureStorageKit {
   ///
   /// return [true] after deleting all variables
   Future<bool> deleteAll() async {
-    await _storage.deleteAll();
+    final FlutterSecureStorage storage = FlutterSecureStorage();
+    await storage.deleteAll();
     return true;
   }
 }
--- test/app_storage_kit_test.dart
+++ test/app_storage_kit_test.dart
@@ -1,13 +1,50 @@
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:app_storage_kit/app_storage_kit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
-  // test('adds one to input values', () {
-  //   final calculator = Calculator();
-  //   expect(calculator.addOne(2), 3);
-  //   expect(calculator.addOne(-7), -6);
-  //   expect(calculator.addOne(0), 1);
-  //   expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  // });
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  String key1 = 'key1';
+  String key2 = 'key2';
+
+  String nsVal1 = 'Normal Storage Val 1';
+  String nsVal2 = 'Normal Storage Val 2';
+
+  String ssVal1 = 'Secure Storage Val 1';
+  String ssVal2 = 'Secure Storage Val 2';
+  group('Normal Storage CRUD', () {
+    SharedPreferences.setMockInitialValues({});
+
+    test('Set and read values into normal storage', () async {
+      String val1 = await NormalStorageKit().readValue(key1);
+      String val2 = await NormalStorageKit().readValue(key2);
+      expect(val1, null);
+      expect(val2, null);
+      bool setState1 = await NormalStorageKit().setValue(nsVal1, key1);
+      bool setState2 = await NormalStorageKit().setValue(nsVal2, key2);
+      expect(setState1, true);
+      expect(setState2, true);
+      val1 = await NormalStorageKit().readValue(key1);
+      val2 = await NormalStorageKit().readValue(key2);
+      expect(val1, nsVal1);
+      expect(val2, nsVal2);
+    });
+
+    test('Delete values from normal storage', () async {
+      String val1 = await NormalStorageKit().readValue(key1);
+      String val2 = await NormalStorageKit().readValue(key2);
+      expect(val1, nsVal1);
+      expect(val2, nsVal2);
+      bool delState1 = await NormalStorageKit().deleteValue(key1);
+      bool delState2 = await NormalStorageKit().deleteValue(key2);
+      expect(delState1, true);
+      expect(delState2, true);
+      val1 = await NormalStorageKit().readValue(key1);
+      val2 = await NormalStorageKit().readValue(key2);
+      expect(val1, null);
+      expect(val2, null);
+    });
+  });
 }
