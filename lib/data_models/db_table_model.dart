203c4fda5ba1b0e4ddf0ab5e38ac8fb2a1591780
Damon Leong <damon.leong@goldhub.hk>
2021-02-25 12:51:30 +0800

    1. database path bug fix 2. added table scheme migration

--- lib/data_models/db_table_model.dart
+++ lib/data_models/db_table_model.dart
@@ -1,12 +1,22 @@
+import 'package:flutter/material.dart';
+
 import 'table_field_model.dart';
 
 class DBTableModel {
-  DBTableModel({this.tableName, this.fields});
+  DBTableModel({
+    @required this.tableName,
+    @required this.fields,
+    this.orgFields,
+  });
 
   final String tableName;
   final List<TableFieldModel> fields;
 
+  /// Support only add new fields
+  final List<TableFieldModel> orgFields;
+
   String get tableQuery => _genCreateTableQuery();
+  String get migrateQuery => _genMigrateQuery();
 
   // Generate SQL to create table
   String _genCreateTableQuery() {
@@ -21,6 +31,21 @@ class DBTableModel {
     });
 
     query += ')';
+    return query;
+  }
+
+  // Generate SQL to update table
+  String _genMigrateQuery() {
+    if (orgFields == null) return '';
+
+    String fields = '';
+    orgFields.forEach((e) {
+      fields += fields == '' ? e.name : ', ${e.name}';
+    });
+
+    String query =
+        'INSERT INTO $tableName($fields) SELECT $fields FROM old_$tableName';
+
     return query;
   }
 }
--- lib/db_storage.dart
+++ lib/db_storage.dart
@@ -15,34 +15,56 @@ class DatabaseStorageKit {
   /// Initialize Database
   Future<Null> initDB() async {
     _dbPath = await getDatabasesPath();
-    _db = await openDatabase(_dbPath + 'appStorageDB.db', version: 1);
-    await _createTable(table);
+    _db = await openDatabase(_dbPath + '/appStorageDB.db', version: 1);
+    await _createTable();
   }
 
   /// Create Table in DB
-  Future<bool> _createTable(DBTableModel table) async {
+  Future<bool> _createTable() async {
     bool createSuccess = true;
-    if (await _ckTableExists(table.tableName)) return createSuccess;
+    if (await _ckTableExists()) return createSuccess;
     try {
       await _db.execute(table.tableQuery);
-    } catch (e) {
+    } catch (e, t) {
+      print("$e - $t");
       createSuccess = false;
     }
     return createSuccess;
   }
 
   /// Check if Table already created
-  Future<bool> _ckTableExists(String tableName) async {
+  Future<bool> _ckTableExists() async {
     List<Map<String, dynamic>> response = await _db.query("sqlite_master",
-        where: 'type = ? AND name = ?', whereArgs: ['table', tableName]);
+        where: 'type = ? AND name = ?', whereArgs: ['table', table.tableName]);
 
-    return response.length > 0 ? true : false;
+    if (response.length <= 0) return false;
+
+    if (response[0]['sql'] != table.tableQuery) await _udpateTable();
+
+    return true;
+  }
+
+  Future<bool> _udpateTable() async {
+    String tempOldTableName = 'old_${table.tableName}';
+
+    try {
+      await _db.execute('DROP TABLE $tempOldTableName');
+    } catch (e, t) {
+      print('$e - $t');
+    }
+
+    await _db
+        .execute('ALTER TABLE ${table.tableName} RENAME TO $tempOldTableName');
+    await _db.execute(table.tableQuery);
+    await _db.execute(table.migrateQuery);
+    await _db.execute('DROP TABLE $tempOldTableName');
+
+    return true;
   }
 
   /// Set / update record
   Future<int> setRecord(DBRecord r, {bool updateIfExists}) async {
     if (!dbReady) await initDB();
-
     int response = await _db.insert(table.tableName, r.toJson(),
         conflictAlgorithm: updateIfExists == false
             ? ConflictAlgorithm.abort
